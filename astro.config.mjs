d4d3ebd94f9a8ea1ed729864419929f5127b636b
Tony Grimes <acidtone@tonygrimes.com>
2024-11-25 09:27:37 -0700

    added web colour, tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,15 +9,15 @@ export default defineConfig({
 			title: 'UX for Devs',
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Visual Hierarchy',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting Started with CSS', slug: 'guides' },
-						{ label: 'Visual Hierarchy', slug: 'guides/visual-hierarchy' },
-						{ label: 'Repetition', slug: 'guides/repetition' },
-						{ label: 'Typography', slug: 'guides/typography' },
-						{ label: 'Space', slug: 'guides/space' },
-						{ label: 'Grid Systems and Alignment', slug: 'guides/grid-systems' },
+						{ label: 'Getting Started with CSS', slug: 'visual-hierarchy' },
+						{ label: 'Typography', slug: 'visual-hierarchy/typography' },
+						{ label: 'Web Colour', slug: 'visual-hierarchy/web-colour' },
+						{ label: 'Repetition', slug: 'visual-hierarchy/repetition' },
+						{ label: 'Space', slug: 'visual-hierarchy/space' },
+						{ label: 'Grid Systems and Alignment', slug: 'visual-hierarchy/grid-systems' },
 					],
 				},
 				{
