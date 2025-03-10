611e83896797c9e5e6311a05a2ced716d98471d7
Tony Grimes <acidtone@tonygrimes.com>
2025-03-10 10:26:17 -0600

    reset content for c14, added visual hierarchy prinicples

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,22 +12,22 @@ export default defineConfig({
 					label: 'Visual Hierarchy',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting Started with CSS', slug: 'visual-hierarchy' },
-						{ label: 'Typography', slug: 'visual-hierarchy/typography' },
+						{ label: 'Introduction to Visual Hierarchy', slug: 'visual-hierarchy' },
+						// { label: 'Typography', slug: 'visual-hierarchy/typography' },
 						{ label: 'Web Colour', slug: 'visual-hierarchy/web-colour' },
-						{ label: 'Repetition', slug: 'visual-hierarchy/repetition' },
-						{ label: 'Space', slug: 'visual-hierarchy/space' },
-						{ label: 'Grid Systems and Alignment', slug: 'visual-hierarchy/grid-systems' },
+						// { label: 'Repetition', slug: 'visual-hierarchy/repetition' },
+						// { label: 'Space', slug: 'visual-hierarchy/space' },
+						// { label: 'Grid Systems and Alignment', slug: 'visual-hierarchy/grid-systems' },
 					],
 				},
-				{
-					label: 'UI Conventions',
-					autogenerate: { directory: 'conventions' }
-				},
-				{
-					label: 'Exercises',
-					autogenerate: { directory: 'exercises' },
-				}
+				// {
+				// 	label: 'UI Conventions',
+				// 	autogenerate: { directory: 'conventions' }
+				// },
+				// {
+				// 	label: 'Exercises',
+				// 	autogenerate: { directory: 'exercises' },
+				// }
 			],
 		}),
 	],
