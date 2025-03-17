38304966801954383bb11ea73b8aba0e52e7ffd7
Tony Grimes <acidtone@tonygrimes.com>
2025-03-17 11:33:03 -0600

    added web colour exercises and resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,13 @@ export default defineConfig({
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Introduction', slug: 'visual-hierarchy' },
 						// { label: 'Typography', slug: 'visual-hierarchy/typography' },
-						{ label: 'Web Colour', slug: 'visual-hierarchy/web-colour' },
+						{
+							label: 'Web Colour',
+							items: [
+								{ label: 'Resources', slug: 'visual-hierarchy/web-colour/resources' },
+								{ label: 'Exercises', slug: 'visual-hierarchy/web-colour/exercises' }
+							],
+						},
 						// { label: 'Repetition', slug: 'visual-hierarchy/repetition' },
 						// { label: 'Space', slug: 'visual-hierarchy/space' },
 						// { label: 'Grid Systems and Alignment', slug: 'visual-hierarchy/grid-systems' },
